eda488eab9e6f4d5c774f9e72d7f75a4f7dd4330
higginbotham-thomas <higginbotham.thomas@icloud.com>
2023-09-25 23:19:20 -0500

    Updating formats
    Date will display as YYYY-MM-DD (with no time), and Close Price will display as a floating point decimal.

--- pm.py
+++ pm.py
@@ -1,13 +1,26 @@
 from flask import Flask, render_template
+from datetime import date, datetime
 import main # Import main.py module
 import pandas as pd
+import locale
+
 
 app = Flask(__name__)
+
+# Set the locale for currency formatting
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
 @app.route('/')
 def display_json_as_table():
     # Call a function from main.py to get JSON data
     json_data = main.results
+
+    for record in json_data:
+        # Convert "close_price" to float before formatting as currency
+        record['close_price'] = float(record['close_price'])
+        # Parse the date string into a date object
+        record['date'] = datetime.strptime(record['date'], '%Y-%m-%d %H:%M:%S')
+
 
     # Assuming you have pandas DataFrame, you can convert it to HTML
     # If your data is not in a DataFrame, you might need to format it accordingly
