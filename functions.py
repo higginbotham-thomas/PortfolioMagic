a302d8ca02f0eb90a5086c95361debe4abad77fa
higginbotham-thomas <higginbotham.thomas@icloud.com>
2023-10-20 17:12:07 -0500

    updating indicators notebook

--- functions.py
+++ functions.py
@@ -1,48 +1,84 @@
-import requests
 import os
-import json
+
+import requests
+import pandas as pd
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
 
 load_dotenv()
 
-FMPKEY = os.getenv('FMPKEY')
+API_KEY = os.getenv('FMPKEY')
 
 
-def get_jsonparsed_data(url):
-    response = requests.get(url)
-    data = response.json()
-    return data
+# Function to compute buy/sell recommendations
+def compute_buy_sell_recommendations(current_portfolio, acct, highest_sharpe_weights, stocks, stock_history):
+    # Convert weights to desired monetary value in the portfolio
+    desired_values = highest_sharpe_weights * acct
+
+    # Get the latest stock prices
+    current_prices = {stock: stock_history[stock].iloc[-1] for stock in stocks}
+
+    # Calculate desired shares (decimal value)
+    desired_shares_decimal = {
+        stock: desired_values[i] / current_prices[stock] for i, stock in enumerate(stocks)}
+
+    # Calculate Buy/Sell (rounded value)
+    buy_sell_shares = \
+        {stock: (desired_shares_decimal[stock]) -
+            current_portfolio[current_portfolio['Symbol'] == stock]['Shares'].values[0] for stock in stocks}
+
+    # Merge current portfolio with desired shares
+    merged_portfolio = current_portfolio.merge(pd.DataFrame(
+        buy_sell_shares.items(), columns=['Symbol', 'Buy/Sell']), on='Symbol')
+
+    # Calculate other columns
+    merged_portfolio['CurrentSharePrice'] = merged_portfolio['Symbol'].map(
+        current_prices)
+    merged_portfolio['TargetValue'] = merged_portfolio['Symbol'].map(
+        {stock: value for stock, value in zip(stocks, desired_values)})
+    merged_portfolio['TargetValue'] = merged_portfolio['TargetValue'].round(2)
+    merged_portfolio['CurrentValue'] = merged_portfolio['Shares'] * \
+        merged_portfolio['CurrentSharePrice']
+    merged_portfolio['CurrentValue'] = merged_portfolio['CurrentValue'].round(
+        2)
+    merged_portfolio['Buy/Sell'] = (merged_portfolio['Buy/Sell']).round(0)
+    merged_portfolio['SharesAfterAction'] = merged_portfolio['Shares'] + \
+        merged_portfolio['Buy/Sell']
+    merged_portfolio['ValueAfterAction'] = merged_portfolio['SharesAfterAction'] * \
+        merged_portfolio['CurrentSharePrice']
+    merged_portfolio['ValueAfterAction'] = merged_portfolio['ValueAfterAction'].round(
+        2)
+    merged_portfolio['TargetSharpeWeight'] = merged_portfolio['Symbol'].map(
+        {stock: weight for stock, weight in zip(stocks, highest_sharpe_weights)})*100  # Convert to percentage
+    merged_portfolio['ActualWeightAfterAction'] = merged_portfolio['ValueAfterAction'] / \
+        (merged_portfolio['ValueAfterAction'].sum()) * \
+        100  # Convert to percentage
+
+    # Sort dataframe by 'Buy/Sell' column with negative values first
+    merged_portfolio = merged_portfolio.sort_values(
+        by='Buy/Sell', ascending=True)  # Sell first, settle, then buy
+
+    return merged_portfolio[['Symbol', 'Buy/Sell', 'CurrentSharePrice', 'CurrentValue', 'Shares', 'TargetValue',
+                             'ValueAfterAction', 'SharesAfterAction', 'TargetSharpeWeight', 'ActualWeightAfterAction']]
 
 
-def analyze_stocks(symbols):
-    results = []
+def get_treasury_data():
+    api_key = API_KEY
+    # Get today's date
+    today = datetime.today()
 
-    for symbol in symbols:
-        sma80_data = get_jsonparsed_data(f'https://financialmodelingprep.com/api/v3/technical_indicator/daily/{symbol}?period=80&type=sma&apikey={FMPKEY}')[0]
-        sma50_data = get_jsonparsed_data(f'https://financialmodelingprep.com/api/v3/technical_indicator/daily/{symbol}?period=50&type=sma&apikey={FMPKEY}')[0]
-        sma20_data = get_jsonparsed_data(f'https://financialmodelingprep.com/api/v3/technical_indicator/daily/{symbol}?period=20&type=sma&apikey={FMPKEY}')[0]
+    # Get yesterday's date
+    yesterday = today - timedelta(days=1)
 
-        date = sma80_data['date']
-        close = sma80_data['close']
-        sma80 = sma80_data['sma']
-        sma50 = sma50_data['sma']
-        sma20 = sma20_data['sma']
+    # Format the dates in 'YYYY-MM-DD' format
+    from_date = yesterday.strftime('%Y-%m-%d')
+    to_date = today.strftime('%Y-%m-%d')
 
-        if float(sma20) > float(sma50) > float(sma80):
-            action = 'BUY'
-        else:
-            action = 'SELL'
+    # Construct the URI
+    uri = f"https://financialmodelingprep.com/api/v4/treasury?from={from_date}&to={to_date}&apikey={api_key}"
 
-        result = {
-            'symbol': symbol,
-            'date': date,
-            'close_price': close,
-            'sma50': sma20,
-            'sma100': sma50,
-            'sma200': sma80,
-            'action': action
-        }
+    # Make the GET request
+    response = requests.get(uri)
 
-        results.append(result)
-
-    return results
+    # Return the JSON response
+    return response.json()
