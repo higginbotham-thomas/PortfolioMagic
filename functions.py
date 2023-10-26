5fe1cd65343a4cb7ce645fe3f8ce942d2a81e39b
Thomas Higginbotham <higginbotham.thomas@icloud.com>
2023-10-26 11:03:00 -0500

    Clearing PEP and Pylint messages

--- functions.py
+++ functions.py
@@ -1,14 +1,10 @@
 """Module with shared functions for the app"""
-import os
-
 from datetime import datetime, timedelta
 import requests
 import pandas as pd
-from dotenv import load_dotenv
-
-load_dotenv()
-
-API_KEY = os.getenv('FMPKEY')
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 
 
 # Function to compute buy/sell recommendations
@@ -68,9 +64,8 @@ def compute_buy_sell_recommendations(
                              'TargetSharpeWeight', 'ActualWeightAfterAction']]
 
 
-def get_treasury_data():
+def get_treasury_data(api_key):
     """Function to get the latest stock prices"""
-    api_key = API_KEY
     # Get today's date
     today = datetime.today()
 
@@ -82,14 +77,11 @@ def get_treasury_data():
         # Otherwise, set from_date to yesterday
         from_date = (today - timedelta(days=1)).strftime('%Y-%m-%d')
     # Format the dates in 'YYYY-MM-DD' format
-    # from_date = yesterday.strftime('%Y-%m-%d')
     to_date = today.strftime('%Y-%m-%d')
 
     # Construct the URI
     base_url = "https://financialmodelingprep.com/api/v4/treasury"
     url = f"?from={from_date}&to={to_date}&apikey={api_key}"
-    # uri = f"https://financialmodelingprep.com/api/v4/treasury?
-    # from={from_date}&to={to_date}&apikey={api_key}"
     uri = f"{base_url}{url}"
 
     # Make the GET request
@@ -97,3 +89,195 @@ def get_treasury_data():
 
     # Return the JSON response
     return response.json()
+
+
+def get_stock_history(stocks, number_of_days, api_key):
+    '''Function to get the latest stock prices'''
+    stock_history = {}
+    base_url = 'https://financialmodelingprep.com/api/v3/historical-price-full/'
+    for stock in stocks:
+        url = f'{base_url}{stock}?serietype=line&apikey={api_key}'
+        try:
+            response = requests.get(url, timeout=10)
+            # Will raise an HTTPError if the HTTP request returned an unsuccessful status code
+            response.raise_for_status()
+            prices = response.json()
+            prices_df = pd.DataFrame(prices['historical'])
+
+            # Ensure the data is sorted by date in descending order (most recent first)
+            prices_df['date'] = pd.to_datetime(prices_df['date'])
+            prices_df = prices_df.sort_values('date', ascending=False)
+
+            # Select closing prices for the chosen number of days
+            recent_prices = prices_df.head(number_of_days).set_index('date')['close']
+            stock_history[stock] = recent_prices
+        except requests.exceptions.HTTPError as http_err:
+            print(f"HTTP error occurred: {http_err}")
+        except requests.exceptions.RequestException as req_err:
+            print(f"Request error occurred: {req_err}")
+        except KeyError:
+            print(f"Error: No data found for stock {stock}")
+
+    return stock_history
+
+
+def get_return_stocks(stock_history):
+    '''Function to get returns for the latest stock prices'''
+    # Concatenate all stock close prices into a single dataframe
+    portfolio = pd.concat(stock_history, axis=1)
+    # Calculate daily returns
+    return_stocks = portfolio.pct_change().dropna()
+    # Return the daily returns and stock history
+    return return_stocks
+
+
+def get_montecarlo_simulation(number_of_portfolios, stocks, return_stocks, trading_days, rf, risk):
+    '''Calculate mean returns and covariances of all assets'''
+    portfolio_returns = []
+    portfolio_risk = []
+    sharpe_ratio_port = []
+    portfolio_weights = []
+
+    for _ in range(number_of_portfolios):
+        weights = np.random.random_sample(len(stocks))
+        weights = weights / np.sum(weights)
+        annualize_return = np.sum((return_stocks.mean() * weights) * trading_days) - rf
+        portfolio_returns.append(annualize_return)
+
+        matrix_covariance_portfolio = (return_stocks.cov()) * trading_days
+        portfolio_variance = np.dot(weights.T, np.dot(matrix_covariance_portfolio, weights))
+        portfolio_standard_deviation = np.sqrt(portfolio_variance)
+        portfolio_risk.append(portfolio_standard_deviation)
+
+        sharpe_ratio = (annualize_return - rf) / portfolio_standard_deviation
+        sharpe_ratio_port.append(sharpe_ratio)
+
+        portfolio_weights.append(weights)
+
+    # Convert lists to arrays outside the loop
+    portfolio_risk = np.array(portfolio_risk)
+    portfolio_returns = np.array(portfolio_returns)
+    sharpe_ratio_port = np.array(sharpe_ratio_port)
+
+    # Filtering portfolios by RISK
+    indices_within_risk = np.where(portfolio_risk <= risk)[0]
+
+    portfolio_risk = portfolio_risk[indices_within_risk]
+    portfolio_returns = portfolio_returns[indices_within_risk]
+    sharpe_ratio_port = sharpe_ratio_port[indices_within_risk]
+    portfolio_weights = np.array(portfolio_weights)[indices_within_risk]
+
+    porfolio_metrics = [portfolio_returns, portfolio_risk, sharpe_ratio_port, portfolio_weights]
+    portfolio_dfs = pd.DataFrame(porfolio_metrics).T
+
+    # Rename the columns:
+    portfolio_dfs.columns = ['Port Returns', 'Port Risk', 'Sharpe Ratio', 'Portfolio Weights']
+
+    # Convert from object to float the first three columns.
+    for col in ['Port Returns', 'Port Risk', 'Sharpe Ratio']:
+        portfolio_dfs[col] = portfolio_dfs[col].astype(float)
+
+    return portfolio_dfs
+
+
+def get_recommendations(portfolio_dfs, current_portfolio, acct, stocks, stock_history):
+    '''Function to get the rebalance recommendations'''
+    highest_sharpe_port = portfolio_dfs.iloc[portfolio_dfs['Sharpe Ratio'].idxmax()]
+    highest_sharpe_weights = highest_sharpe_port['Portfolio Weights']
+
+    # Get buy/sell recommendations
+    recommendations = compute_buy_sell_recommendations(
+        current_portfolio, acct, highest_sharpe_weights, stocks, stock_history)
+
+    recommendations = recommendations.loc[(recommendations['CurrentValue']
+                                           != recommendations['ValueAfterAction']) |
+                                           ((recommendations['CurrentValue'] != 0) &
+                                            (recommendations['ValueAfterAction'] != 0))]
+
+    return recommendations
+
+
+def plot_portfolio_metrics(portfolio_dfs, number_of_portfolios, acct, rf, risk):
+    '''Function to plot the portfolio metrics (returns, risk, sharpe ratio)'''
+    highest_sharpe_port = portfolio_dfs.iloc[portfolio_dfs['Sharpe Ratio'].idxmax()]
+
+    # First Graphic: Text String Block
+    _, ax1 = plt.subplots(figsize=(15, 5))
+
+    # Add text annotations
+    textstr = "Portfolio Metrics for the Highest Sharpe Ratio Portfolio:\n\n"
+    textstr += f"Number of portfolios analyzed: {number_of_portfolios}\n"
+    textstr += f"Returns: {highest_sharpe_port['Port Returns'] * 100:.2f}%\n"
+    textstr += f"Risk (Standard Deviation): {highest_sharpe_port['Port Risk'] * 100:.2f}%\n"
+    textstr += f"Sharpe Ratio: {highest_sharpe_port['Sharpe Ratio']:.2f}\n\n"
+    textstr += f"Portfolio Value: ${acct:.2f}\n"
+    textstr += f"Risk Free Rate: {rf * 100:.2f}%\n\n"
+    textstr += f"Selected Risk Tolerance: {risk * 100:.2f}%:\n"
+
+    # Place the text on the figure
+    ax1.text(0.05, 0.7, textstr, transform=ax1.transAxes, fontsize=12, verticalalignment='top',
+             horizontalalignment='left')
+
+    # Hide the axes
+    ax1.axis('off')
+
+    # Show the figure
+    plt.show()
+
+
+def plot_portfolio_changes(recommendations):
+    '''Function to plot the portfolio changes'''
+
+    # Round and format the values in the specified columns
+    recommendations['Shares'] = recommendations['Shares'].apply(lambda x: f"{x:.4f}")
+    recommendations['SharesAfterAction'] = recommendations['SharesAfterAction'].apply(lambda x: f"{x:.4f}")
+    recommendations['TargetSharpeWeight'] = recommendations['TargetSharpeWeight'].round(2)
+    recommendations['ActualWeightAfterAction'] = recommendations['ActualWeightAfterAction'].round(2)
+
+    # Second Graphic: Table of Recommendations
+    _, ax2 = plt.subplots(figsize=(15, 5))
+
+    # Create a table for the recommendations
+    table_data = []
+    columns = recommendations.columns.tolist()
+    table_data.append(columns)
+    for row in recommendations.itertuples():
+        table_data.append(row[1:])
+
+    # Add table to the figure
+    _ = ax2.table(cellText=table_data, loc='center', cellLoc='center', colWidths=[0.2] * len(columns),
+                      bbox=[0, 0, 1, 1], fontsize=18)
+
+    # Hide the axes
+    ax2.axis('off')
+
+    # Show the figure
+    plt.show()
+
+
+def plot_risk_scatterplot(portfolio_dfs):
+    '''Function to reate a dataframe with the returns and risk value for each portfolio'''
+    portfolio_returns = portfolio_dfs['Port Returns']
+    portfolio_risk = portfolio_dfs['Port Risk']
+
+    plt.figure(figsize=(15, 15))
+    plt.scatter(portfolio_risk, portfolio_returns, c=portfolio_returns / portfolio_risk)
+    plt.xlabel('Volatility (Risk)')
+    plt.ylabel('Returns')
+    plt.colorbar(label='Sharpe ratio')
+    plt.title('Tested Portfolios (Returns vs Volatility)')
+
+    plt.show()
+
+
+def plot_correlation_matrix(return_stocks):
+    '''Function to plot the correlation matrix'''
+    
+    # Calculate the correlation matrix
+    correlation_matrix = return_stocks.corr()
+
+    # Visualize the correlation matrix using a heatmap
+    plt.figure(figsize=(15, 15))
+    sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", linewidths=0.5)
+    plt.title("Correlation Matrix of Asset Returns")
+    plt.show()
